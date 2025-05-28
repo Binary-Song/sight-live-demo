d839afb1991e0a10c4091621ea59a0d0f7959e86
binary-song <yeh189@126.com>
2025-05-28 21:51:44 +0800

    Add stuff

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+mod utils;
+use sight_lang::typing::type_term_with_source;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn type_source(source: &str) -> Option<String> {
+    use sight_lang::parser::parse;
+    wasm_logger::init(wasm_logger::Config::default());
+
+    let term = parse(source).unwrap();
+    let ty = type_term_with_source(term, source)?;
+    Some(format!("{ty:?}"))
+}
